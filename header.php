3b23610d847cb1ff74d111489bdbd687f76ac225
Marcus Rowland <marcusrowland@Marcuss-MacBook-Pro.local>
2015-02-04 13:51:20 +0000

    fixed jquery, where I forgot how to program

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="scripts/css.css">
-  <script type="text/javascript" src="scripts/jquery-1.11.2.min.js" />
+  <link rel="stylesheet" href="scripts/style.css">
+  <script type="text/javascript" src="scripts/jquery-1.11.2.min.js"></script>
 </head>
 <body>
   <h1>NSU:Media</h1>
--- index.php
+++ index.php
@@ -2,6 +2,21 @@
 include_once('connect.php');
 include('header.php');
 ?>
+
+
+<script>
+$(document).ready(function() {
+  $('#email').on('input', function() {
+    var input=$(this);
+    var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var is_email=re.test(input.val());
+    if(is_email){input.removeClass("invalid").addClass("valid");}
+    else{input.removeClass("valid").addClass("invalid");}
+  });
+});
+</script>
+
+
 <!--Login form-->
 <div>
   <form name="login" action="userFunction.php" method="post">
@@ -15,7 +30,7 @@ include('header.php');
   <form name="register" action="userFunction.php" method="post">
     First name: <input type="text" name="fname"><br>
     Surname: <input type="text" name="sname"><br>
-    Email: <input type="text" name="email"><br><!--live check required-->
+    Email: <input type="text" id="email" name="email"><br><!--live check required-->
     Username: <input type="text" name="username"><br>
     Password: <input type="password" name="password"><br> <!-- live checking-->
     <input type="submit" value="Register">
