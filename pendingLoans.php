b8e1ea90c1603dfc8e4afae587c8e6096947f020
Marcus Rowland <marcusrowland@Marcuss-MacBook-Pro.local>
2015-02-11 22:23:36 +0000

    adding a briefing page and email

---
+++ pendingLoans.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+include_once('connect.php');
+include('header.php');
+?>
+<div class="container">
+  <h2>Pending Loans</h3>
+    <p>Here you can manage any pending loans that are due to be approved or checked-out</p>
+</div>
+<?php
+$i=0;
+
+$selectQuery="SELECT loan.loanNumber, count, plannedStart, plannedEnd, user.UID, firstname, surname
+FROM loan INNER JOIN user ON loan.UID=user.UID
+WHERE actualStart IS NULL";
+
+if($results=mysqli_query($conn, $selectQuery)){
+  while($row=mysqli_fetch_row($results)){
+    //display normal results
+    $loanNumber=$row[0];
+    $count = $row[1];
+    echo "Number of Requested Items: ".$count;
+    echo "<form name='ajaxform".$i."' id='ajaxform".$i."' action='ajax-form.php' method='POST'>";
+    echo "Planned Start: ".$row[2]." Planned End: ".$row[3]."<br/>\n";
+    echo "Name: ".$row[5]." ".$row[6]."</br>\n";
+    $sql="SELECT loantoasset.barcode, make, model
+    FROM loantoasset INNER JOIN assets ON loantoasset.barcode = assets.barcode
+    WHERE loanNumber='$loanNumber'";
+    $result=mysqli_query($conn, $sql);
+    if($count > 1){
+        while($assetRow=mysqli_fetch_array($result)){
+          $barcode=$assetRow[0];
+          echo "Barcode: ".$barcode."</br>\n";
+          echo "Make: ".$assetRow['make']." Model: ".$assetRow['model']."<br/>\n";
+        }
+    }
+    else {
+      while($singleassetRow=mysqli_fetch_array($result)){
+      echo "Barcode: ".$singleassetRow[0]."</br>\n";
+      echo "Make: ".$singleassetRow['make']." Model: ".$singleassetRow['model']."<br/>\n";
+    }
+      //mysqli_free_result($result);
+    }
+    echo "<input type='submit' value='Approve' id='approve".$i."' class='btn btn-success btn-sml' />";
+    echo "<input type='submit' value='Reject' id='reject".$i."' class='btn btn-danger btn-sml' />";
+    echo "</form><br/>\n";
+    $i++;
+  }
+  //mysqli_free_result($results);
+}
+else {
+  echo "Error: ".$selectQuery." ".$conn->error;
+}
+?>
+</div>
--- sendRequest.php
+++ sendRequest.php
@@ -56,9 +56,6 @@ $headers = "From: ".$email_from."\r\n".
 "Reply-To: ".$email_from."\r\n".
 "X-Mailer: PHP/".phpversion();
 mail($email_to, $email_subject, $email_message, $headers);
-
-echo $uid;
-echo $email_from;
 ?>
 
 <div class="container">
