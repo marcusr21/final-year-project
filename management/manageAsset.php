a12bfcfaaa7425ec01057f4de36e36787952653e
Marcus Rowland <marcusrowland@Marcuss-MacBook-Pro.local>
2015-02-24 11:50:48 +0000

    Editing script complete

--- management/manageAsset.php
+++ management/manageAsset.php
@@ -8,15 +8,21 @@ $headerPath .= $path;
 $headerPath .= "/header.php";
 include($connectPath);
 include($headerPath);
+
+if(isset($_REQUEST['type'])){
+  echo $_REQUEST['id']." has been ".$_REQUEST['type'];
+}
 ?>
 <script>
 $(document).ready(function(){
 
   $("#formSelect").on("change", function() {
       $("#" + $(this).val()).show().siblings().hide();
+      $('#formShow').show();
   })
 
   $('#editSearch').on('input', function(){
+    $('#content').show();
     var searchKeyword= $(this).val();
     $.post('/../search.php', {keywords: searchKeyword}, function(data) {
       $('#content').empty();
@@ -37,10 +43,11 @@ $(document).ready(function(){
         dataType: 'json',
         success: function(data) {
           $('.editForm').html(
-            'Make <input type="text" id="editModel" value="'+data["make"]+'"><br/>Model: <input type="text" id="editModel" value="'+data["model"]+'"><br/> Description: <input type="textarea" value="'+data["description"]+'"><br/>Tags: <input type="text" id="editTags" value="'+data["tags"]+'"><br/> Category: <input type="text" id="editCategory" value="'+data["category"]+'"><input type="hidden" value="'+data["barcode"]+'">'
+            'Make <input type="text" id="make" name="make" value="'+data["make"]+'"><br/>Model: <input type="text" name="model" id="model" value="'+data["model"]+'"><br/> Description: <input type="textarea" name="desc" value="'+data["description"]+'"><br/>Tags: <input type="text" id="tags" name="tags" value="'+data["tags"]+'"><br/> Category: <input type="text" id="category" name="category" value="'+data["category"]+'"><input type="hidden" id="barcode" name="barcode" value="'+data["barcode"]+'">'
           );
           $('#editSubmit').css('visibility', 'visible');
           $('#content').hide();
+          $('.searchBox').hide();
         }
       });
     });
@@ -48,10 +55,36 @@ $(document).ready(function(){
     $(document).ready(function(){
       $("a").trigger("click");
     });
+
+    $('#editSearchAgain').click(function(event){
+      event.preventDefault();
+      event.unbind();
+      $('.searchBox').show();
+      $('.editForm').hide();
+      $('#editSubmit').css('visibility', 'hidden');
+    });
+
+    /*$('#edit').submit(function(event){
+
+      var postData=$(this).serializeArray();
+      var formURL=$(this).attr('action');
+      $.ajax({
+        url: formURL,
+        type: 'POST',
+        data: postData,
+        success: function(data){
+          $('.editForm').hide();
+          $('#editSubmit').css('visibility', 'hidden');
+          $('#content').html('<p>'+data['result']+'</p>');
+        }
+      })
+      event.preventDefault();
+      event.unbind();
+    });*/
 });
 </script>
 <div class='container'>
-  <form id='form-show'>
+  <form id='formShow'>
     <select id='formSelect'>
       <option value='' selected='selected'></option>
       <option value='add'>Add an asset</option>
@@ -62,8 +95,8 @@ $(document).ready(function(){
 
   <form id='add' name='add' method='POST' action='update.php' style='display:none'>
     Asset Number: <input type='text' id='addBarcode' />
-    Make: <input type='text' id='addMake' />
-    Model: <input type='text' id='addModel' />
+    Make: <input type='text' id='make' />
+    Model: <input type='text' id='model' />
     Description: <input type='textarea' />
     Tags: <input type='text' id='tags' />
     Category: <select id='category'>
@@ -81,14 +114,16 @@ $(document).ready(function(){
     <input type='submit' value='Add Asset' class='btn btn-primary btn-sml' />
   </form>
 
-  <form id='edit' name='edit' method='POST' action='update.php' style='display:none'>
-    Search for Asset: <input type='text' id='editSearch' />
+  <form id='edit' name='edit' method='POST' action='updateAsset.php' style='display:none'>
+    <div class='searchBox'>
+      Search for Asset: <input type='text' id='editSearch' />
+    </div>
+    <button id='editSearchAgain'>Search Again</button>
     <div id='content'>
     </div>
     <div class='editForm'>
     </div>
-    <!-- AJAX to lokup asset to be edited-->
-    <input type='hidden' id='type' value='edit' />
+    <input type='hidden' id='type' name='type' value='edit' />
     <input type='submit' class='btn btn-primary btn-sml' id='editSubmit' value='Edit Asset' style='visibility:hidden' />
   </form>
 
--- management/updateAsset.php
+++ management/updateAsset.php
@@ -5,9 +5,26 @@ $connectPath .= $path;
 $connectPath .= "/connect.php";
 include($connectPath);
 
+$barcode=$_POST['barcode'];
+$make=$_POST['make'];
+$model=$_POST['model'];
+$tags=$_POST['tags'];
+$desc=$_POST['description'];
+$cat=$_POST['category'];
+
 if(isset($_POST['type']) && $_POST['type']=='edit'){
-  $barcode=$_POST['barcode'];
-  $sql=
+  $sql="UPDATE assets
+  SET barcode='$barcode', make='$make', model='$model', tags='$tags', description='$desc', category='$cat'
+  WHERE barcode='$barcode'";
+  if(mysqli_query($conn, $sql) === TRUE){
+    //$array=array('result'=>'Asset updated');
+    header('Location: manageAsset.php?type=edit&id='.$barcode);
+  }
+  else{
+    //$array=array('result'=>'Update Failed');
+    echo "Error ".$conn->error;
+  }
+  //echo json_encode($array);
 }
 elseif(isset($_POST['type']) && $_POST['type']=='add'){
   //do add here
