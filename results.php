a46728e989a6d5a726e5e94b457d5b36ed8136b2
Marcus Rowland <marcusrowland@Marcuss-MacBook-Pro.local>
2015-03-09 13:21:51 +0000

    Fixing issue #1 with db rewrite and code rewrite. Issue #1 closed

--- results.php
+++ results.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
-$search=$_GET['q'];
-$barcode=$_REQUEST['id'];
+$search=isset($_GET['q']) ? $_GET['q'] : "";
+$barcode=isset($_REQUEST['id']) ? $_REQUEST['id'] : null;
+
 $first=$_SESSION['first'];
-?>
-<?php
 $current_url = $_SERVER['REQUEST_URI'];
 $i=0;
 include('header.php');
 include('connect.php');
+//require_once('search.php');
 
 if($search=="" && $barcode ==""){
   $searchQuery="SELECT assets.id, make, model, tags, category.category, description FROM assets INNER JOIN category
@@ -21,7 +21,6 @@ if($search=="" && $barcode ==""){
       $model[$i]=$row[2];
       $category[$i]=$row[4];
       $desc[$i]=$row[5];
-      $tags[$i]=explode(", ", $row[3]);
       $i++;
     }
   }
@@ -31,9 +30,10 @@ if($search=="" && $barcode ==""){
   $(document).ready(function(){
 
     $('#startDate').datepicker({
-      defaultDate: "+1w",
+      defaultDate: "1+w",
       numberOfMonths: 1,
       dateFormat: "dd-mm-yy",
+      minDate: 0,
       onClose: function(selectedDate) {
         $('#endDate').datepicker("option", "minDate", selectedDate);
         var startDateVal=$('#startDate').datepicker("getDate");
@@ -51,7 +51,7 @@ if($search=="" && $barcode ==""){
       }
     });
     $('#endDate').datepicker({
-      defaultDate: "1+w",
+      defaultDate: "2+w",
       numberOfMonths: 1,
       dateFormat: "dd-mm-yy",
       onClose: function(selectedDate) {
@@ -72,7 +72,7 @@ if($search=="" && $barcode ==""){
       }
     });
 
-    //$('#tags').select2();
+    $('#tags').select2();
 
         /*$('#shoppingBasket').affix({
           offset: {
@@ -93,13 +93,13 @@ if($search=="" && $barcode ==""){
     <?php
     if(isset($_SESSION['products'])){
       $total=0;
+      echo '<div class="startDate">Start Date: '.$start.'</div>';
+      echo '<div class="endDate">End Date: '.$end.'</div>';
       echo '<ol>';
       foreach($_SESSION['products'] as $item){
         echo '<li class=cart-item>';
         echo '<strong>'.$item["make"].' '.$item["model"].' </strong>';
         echo '<span class="errorMessage">'.$item["message"].'</span>';
-        echo '<div class="startDate">Start Date: '.$item["start"].'</div>';
-        echo '<div class="endDate">End Date: '.$item["end"].'</div>';
         echo '<a class="btn btn-danger btn-sml" href="basket_update.php?remove='.$item["barcode"].'&returnurl='.$current_url.'">Remove this item</a>';
         echo '</li>';
       }
@@ -125,8 +125,6 @@ if($search=="" && $barcode == ""){
       echo "<input type='submit' value='Add to Basket' class='btn btn-primary btn-sml'>\n";
       echo "<input type='hidden' name='barcode' value='".$barcodeArray[$count]."' />\n";
       echo "<input type='hidden' name='url' value='".$current_url."' />\n";
-      echo "<input type='hidden' name='start' id='start' value='' />\n";
-      echo "<input type='hidden' name='end' id='end' value='' />\n";
       echo "<input type='hidden' name='type' value='add' />\n";
       echo "</form>\n";
       echo "</div>\n";
@@ -152,8 +150,6 @@ elseif($barcode!=""){
       echo "<button class='btn btn-primary btn-sml'>Add to basket</button>\n";
       echo "<input type='hidden' name='barcode' value='".$barcode[$i]."' />\n";
       echo "<input type='hidden' name='url' value='".$current_url."' />\n";
-      echo "<input type='hidden' name='start' id='start' value=''>\n";
-      echo "<input type='hidden' name='end' id='end' value=''>\n";
       echo "<input type='hidden' name='type' value='add' />\n";
       echo "</form>\n";
       echo "</div>\n";
@@ -162,30 +158,59 @@ elseif($barcode!=""){
   }
 }
 if($search!=""){
-  $selectResults="SELECT assets.id, make, model, category.category, description, MATCH (make, model, description, tags) AGAINST ('*$search*' IN BOOLEAN MODE) as relevant
+  $selectResults="SELECT assets.id, make, model, category.category, description, tags.tag, MATCH (make, model, description, tags) AGAINST ('*$search*' IN BOOLEAN MODE) as relevant
   FROM assets INNER JOIN category
   ON assets.category=category.id
+  INNER JOIN assettotag ON assets.id=assettotag.assetid
+  INNER JOIN tags ON assettotag.tagid=tags.tagid
   WHERE MATCH (make, model, description, tags) AGAINST ('*$search*' IN BOOLEAN MODE)
   ORDER BY relevant DESC";
   if($results=mysqli_query($conn, $selectResults)){
     $i=0;
     while($row=mysqli_fetch_array($results)){
       $category[$i]=$row['category'];
-      $tags[$i]=explode(", ", $row['tags']);
-      echo "<div class='form-group'>\n";
-      echo "<form method='post' action='basket_update.php'>\n";
-      echo "Make: ".$row['make']."<br> Model: ".$row['model']."<br>\n";
-      echo "Category: ".$row['category']."<br>\n";
-      echo "Description: ".$row['description']."<br>\n";
-      echo "<button class='btn btn-primary btn-sml'>Add to basket</button>\n";
-      echo "<input type='hidden' name='barcode' value='".$row['id']."' />\n";
-      echo "<input type='hidden' name='url' value='".$current_url."' />\n";
-      echo "<input type='hidden' name='start' id='start' value=''>\n";
-      echo "<input type='hidden' name='end' id='end' value=''>\n";
-      echo "<input type='hidden' name='type' value='add' />\n";
-      echo "</form>\n";
-      echo "</div>\n";
+      $tags[$i]=$row['tag'];
+      $id[]=$row['id'];
       $i++;
+    }
+      $tmpArr = array_unique($id);
+      foreach($tmpArr as $v){
+        $newArr[$count]=$v;
+        $count++;
+      }
+      foreach($newArr as $value){
+        $sql="SELECT * FROM assets INNER JOIN category
+        ON assets.category=category.id
+        WHERE assets.id='$value'";
+        $result=mysqli_query($conn, $sql);
+        while($rows=mysqli_fetch_array($result)){
+          $barcode[]=$rows[0];
+          echo "<div class='form-group'>\n";
+          echo "<form method='post' action='basket_update.php'>\n";
+          echo "Make: ".$rows['make']."<br> Model: ".$rows['model']."<br>\n";
+          echo "Category: ".$rows['category']."<br>\n";
+          if(isset($_SESSION['products'])){
+            $flag=false;
+            foreach($_SESSION['products'] as $itemArray){
+              if($itemArray['barcode']==$rows[0]){
+                $flag=true;
+                echo '<a class="btn btn-danger btn-sml" href="basket_update.php?remove='.$itemArray["barcode"].'&returnurl='.$current_url.'">Remove this item</a><br/>';
+              }
+            }
+            if($flag==false){
+              echo "<button class='btn btn-primary btn-sml'>Add to basket</button><br/>\n";
+            }
+          }
+          else{
+            echo "<button class='btn btn-primary btn-sml'>Add to basket</button><br/>\n";
+          }
+          echo "Description: ".$rows['description']."<br>\n";
+          echo "<input type='hidden' name='barcode' value='".$rows[0]."' />\n";
+          echo "<input type='hidden' name='url' value='".$current_url."' />\n";
+          echo "<input type='hidden' name='type' value='add' />\n";
+          echo "</form>\n";
+          echo "</div>\n";
+        }
     }
   }
   else{
@@ -198,30 +223,24 @@ if($search!=""){
   <div id="resultFilter" data-spy="affix" class="navbar">
     Start Date: <input type="text" id="startDate">
     End Date: <input type="text" id="endDate">
-    <form id='filters' name='filters'>
+    <form id='filters' name='filters' method="POST" action="search.php">
       <?php
       //get all possible filter info from previous functions
       $uniqueCategory=array_unique($category);
-      //$tagsExplode=explode(" , ", $tags);
-      //$uniqueTags=array_unique($tags);
-      $keys = array_keys($tags);
-      for($count=0; $count < count($tags[$keys[0]]); $count++){
-        $data=array();
-        foreach($tags as $key => $value){
-          $data[$key] = $value[$count];
-        }
-      }
-      $uniqueTags=array_unique($data);
+      $uniqueTags=array_unique($tags);
       foreach($uniqueCategory as $cat){
         echo "<input type='radio' name='category' value='".$cat."'>".$cat."</br>\n";
       }
-      echo "Tags <select id='tags' multiple>\n";
+      echo "Tags <select name='tags[]' id='tags' multiple>\n";
       echo "<option></option>";
       foreach($uniqueTags as $tag){
         echo "<option value='".$tag."'>".$tag."</option>\n";
       }
       echo "</select><br/>\n";
-      echo "<input type='submit' value='Search!' class='btn btn-primary btn-sml'>";
+      echo "<input type='hidden' id='url' value='".$current_url."' />\n";
+      echo "<input type='hidden' id='name' name='name' value='advanced' />\n";
+      echo "<input type='hidden' id='query' name='query' value='".$q."' />\n";
+      echo "<input type='submit' value='Search!' class='btn btn-primary btn-sml'>\n";
       ?>
     </form>
   </div>
--- search.php
+++ search.php
@@ -1,9 +1,20 @@
 <?php
 require_once('connect.php');
+$formName=$_POST['name'];
+$q=isset($_POST['query']) ? $_POST['query'] : null;
+$tags=isset($_POST['tags']) ? $_POST['tags'] : null;
+$cat=isset($_POST['category']) ? $_POST['category'] : null;
+
+if($formName='advanced'){
+  advancedSearch($q, $tags, $cat);
+}
+else{
     $array=array();
     $key=$_POST['keywords'];
-    $sql="SELECT * FROM assets
-    WHERE MATCH (make, model, description, tags) AGAINST ('*$key*' IN BOOLEAN MODE)";
+    $sql="SELECT *, MATCH (make, model, description, tags) AGAINST ('*$key*' IN BOOLEAN MODE) as relevant LIMIT 5
+    FROM assets
+    WHERE MATCH (make, model, description, tags) AGAINST ('*$key*' IN BOOLEAN MODE)
+    ORDER BY relevant DESC";
     $query=mysqli_query($conn, $sql);
       while($row=mysqli_fetch_array($query))
       {
@@ -11,4 +22,18 @@ require_once('connect.php');
       }
 
     echo json_encode($array);
+}
+
+function advancedSearch($query, $tagParam, $category){
+      $searchResults[]=array();
+      $w = array();
+
+      foreach($tagParam as $tag){
+        $w[] = "tags.tag='$tag'";
+      }
+      $w[]="category='$category'";
+
+      
+      return $searchResults;
+    }
 ?>
