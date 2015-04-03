278ac48ae1cf8624b8c9ee3db4f630e34dd44821
Marcus Rowland <marcusrowland@Marcuss-MacBook-Pro.local>
2015-04-03 12:51:08 +0100

    Password reset complete, now to allow normal password changes

--- scripts/changePassword.php
+++ scripts/changePassword.php
@@ -1,8 +1,5 @@
 <?php
 session_start();
-$path = $_SERVER['DOCUMENT_ROOT'];
-$redirectPath .= $path;
-$redirectPath .= "/splash.php";
 
 $path = $_SERVER['DOCUMENT_ROOT'];
 $connectPath .= $path;
@@ -19,13 +16,13 @@ if($type=='reset'){
   $sql="SELECT token FROM user WHERE token='$token' LIMIT 1";
   $result=mysqli_query($conn, $sql);
   $number=mysqli_num_rows($result);
-  if($number=0){
+  if($number==0){
     header("Location: ../index.php");
   }
   else{
     $updatePassword="UPDATE user SET password='$hash' WHERE token='$token'";
     if($result=mysqli_query($conn, $updatePassword)){
-      $selectUser="SELECT UID, username, password, firstname FROM user WHERE token = '$token' LIMIT 1";
+      $selectUser="SELECT UID, username, password, firstname FROM user WHERE token='$token' LIMIT 1";
       $selectResult=mysqli_query($conn, $selectUser);
       while($row=mysqli_fetch_array($selectResult)){
         $UID=$row['uid'];
