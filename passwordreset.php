c859b8450f5e394646e35961a04d164dd036b1f3
Marcus Rowland <marcusrowland@Marcuss-MacBook-Pro.local>
2015-04-03 13:28:12 +0100

    Making sure passwords match

--- passwordreset.php
+++ passwordreset.php
@@ -1,19 +1,38 @@
 <?php
 include('connect.php');
 include('header.php');
+?>
 
+<script>
+  $(document).ready(function(){
+    $("#checkPassword").on('input', function(){
+      var input=$(this);
+      var origin=$("#newPassword");
+      if(origin.val()==input.val()){input.removeClass("invalid").addClass("valid");}
+      else{input.removeClass("valid").addClass("invalid");}
+    });
+
+    $("#change").click(function(e){
+      if($("#checkPassword").hasClass("invalid")){
+        alert("Please ensure passwords match");
+        e.preventDefault(e);
+      }
+    });
+  });
+</script>
+
+<?php
 $token=$_GET['token'];
 
 if(isset($_GET['token'])){
   echo "<div class='container'>\n";
   echo "<form name='reset' action='scripts/changePassword.php' method='POST'>\n";
-  echo "New Password: <input type='text' id='newPassword' />\n";
-  echo "Repeat Password: <input type='text' id='checkPassword' />\n";
+  echo "New Password: <input type='password' id='newPassword' />\n";
+  echo "Repeat Password: <input type='password' id='checkPassword' />\n";
   echo "<input type='hidden' name='value' id='value' value='".$token."' />\n";
   echo "<input type='hidden' id='type' name='type' value='reset' />\n";
-  echo "<button type='submit' class='btn btn-default btn-sml'>Change Password</button>\n";
+  echo "<button type='submit' id='change' class='btn btn-default btn-sml'>Change Password</button>\n";
   echo "</form>\n";
   echo "</div>\n";
 }
-
 ?>
