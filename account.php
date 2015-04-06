bc2e50b08a8c17aacda4836b0797d51a1bf07752
Marcus Rowland <marcusrowland@Marcuss-MacBook-Pro.local>
2015-04-06 22:58:44 +0100

    Minor chages

--- account.php
+++ account.php
@@ -54,22 +54,22 @@ if(isset($passwordStatus)){
 <div class='container'>
   <h2>Manage Account</h2>
   <p>Below you can manage your account and change any details</p>
-  <div class="option">
+  <div class="accountOption">
     <h3><a href="#" id='details'>Change Details</a></h3>
   </div>
-  <div class='option'>
+  <div class='accountOption'>
     <h3><a href="#" id="password">Change Password</a></h3>
   </div>
 
-  <form id="changeDetails" action="management/updateUser.php" method="POST" style="visibility:hidden">
-    <label for="username">Username</label><input type='text' name='user' value=<?php echo $username ?> />
-    <label for="firstname">Firstname</label><input type='text' name="first" value=<?php echo $first ?> />
-    <label for="surnameLabel">Surname</label><input type='text' name='surname' value=<?php echo $surname ?> />
-    <label for="emailLabel">Email</label><input type='text' name='email' value=<?php echo $email ?> />
+  <form id="changeDetails" action="management/updateUser.php" method="POST" style="display:none">
+    Username<input type='text' name='user' value=<?php echo $username ?> />
+    Firstname<input type='text' name="first" value=<?php echo $first ?> />
+    Surname<input type='text' name='surname' value=<?php echo $surname ?> />
+    Email<input type='text' name='email' value=<?php echo $email ?> />
     <button type='submit' class='btn btn-primary btn-sml'>Update Details</button>
   </form>
 
-  <form id="updatePassword" action="scripts/changePassword.php" method="POST" style="visibility:hidden">
+  <form id="updatePassword" action="scripts/changePassword.php" method="POST" style="display:none">
     <label for="oldPassLabel">Current Password</label><input type='password' name='oldPass' id='oldPass' />
     <label for="newPass">New Password</label><input type='password' name='originPass' id='originPass' />
     <label for="repeat">Repeat Password</label><input type='password' name='checkPassword' id='repeatPass' />
